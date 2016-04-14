5779b3ca4a94d8c333543bc6b580d905d62ba508
shradha09 <shradhakhare.9@gmail.com>
2016-04-14 14:28:51 +0530

    files added

--- rsvptest.py
+++ rsvptest.py
@@ -2,22 +2,27 @@ import os
 import json
 import string
 import csv
+import io
 from flask import Flask, render_template, redirect, url_for, request,make_response
 from pymongo import MongoClient
 from bson.json_util import dumps
+import socket
 
 app = Flask(__name__)
 
-client = MongoClient('192.168.99.100', 27017)
+client = MongoClient('mongodb',27017)
 db = client.rsvpdata
+
 
 @app.route('/')
 def rsvpdata():
 	_items = db.rsvpdata.find()
 	items = [item for item in _items]
 	count = len(items)
-	print(items)
-	return render_template('profile.html',counter=count, items=items)
+	
+	hostname = socket.gethostname()
+
+	return render_template('profile.html',counter=count,hostname=hostname, items=items)
 	
 
 @app.route('/new', methods=['POST'])
@@ -36,9 +41,36 @@ def csvdata():
 	k = dumps(_items)
 	j = json.dumps(k)
 	
-    	response = make_response(j)
-    	response.headers["Content-Disposition"] = "attachment; filename=rsvp.json"
+	response = make_response(j)
+	response.headers["Content-Disposition"] = "attachment; filename=rsvp.json"
 	return response
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
+
+
+	
+ #read in the JSON file into Python as a string
+#	rsvp_json =  open("/home/skhare/Downloads/rsvp.json").read()
+#	
+#	print(rsvp_json)
+#	rsvp_parsed = json.loads(rsvp_json)
+#        print (rsvp_parsed)
+#	rsvpfile = open("/home/skhare/Downloads/rsvp.csv","r+")
+#	
+#	csvwriter = csv.writer(rsvpfile)
+#	count = 0
+#
+#	for rsvp1 in rsvpfile:
+#		if count == 0:
+#			header = rsvp1.keys()
+#			csvwriter.writerow(header)
+#			count += 1 
+#			csvwriter.writerow(rsvp1.values())
+#
+#	rsvpfile.close()
+	
+
+
+
